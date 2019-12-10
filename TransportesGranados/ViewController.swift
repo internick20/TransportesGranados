9500bf11b05471596eeb0558f3ec2d50020e977c
Jose Trinidad <internick18@gmail.com>
2019-12-10 14:00:14 -0600

    Initial Commit

---
+++ TransportesGranados/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransportesGranados
+//
+//  Created by Admin on 10/12/19.
+//  Copyright © 2019 galosoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
